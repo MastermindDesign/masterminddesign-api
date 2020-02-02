182fd71e182f9dc14b427a0e3eb53b277d4672da
fr0nk3nst33n <josh@masterminddesign.io>
2020-02-01 21:54:40 -0700

    Added Elastic Logging

--- project/repo.go
+++ project/repo.go
@@ -6,13 +6,16 @@ import (
 
 	"github.com/FR0NK3NST33N/masterminddesign-api/data"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func FindAll() []Project {
 	ctx := context.Background()
+	findOptions := options.Find()
+	findOptions.SetSort(bson.D{{"order", 1}})
 	var results []Project
 	coll := data.Mongo.Database(data.DB).Collection("projects")
-	cursor, err := coll.Find(ctx, bson.M{})
+	cursor, err := coll.Find(ctx, bson.D{}, findOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
